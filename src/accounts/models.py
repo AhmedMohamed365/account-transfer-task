9312d7ffd22250623e82243124d716bdcb745529
Ahmed Mohamed Hassan <56398966+AhmedMohamed365@users.noreply.github.com>
2025-01-14 22:35:45 +0200

    feat: Add Transaction table to save transactions

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -13,3 +13,12 @@ class Account(models.Model):
     
     # def display_balance(self):
     #     return f'{self.name} - Balance: {self.balance}'
+
+class Transaction(models.Model):
+    from_account = models.ForeignKey(Account, related_name='outgoing_transactions', on_delete=models.CASCADE)
+    to_account = models.ForeignKey(Account, related_name='incoming_transactions', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Transaction from {self.from_account.name} to {self.to_account.name} - Amount: {self.amount}'
